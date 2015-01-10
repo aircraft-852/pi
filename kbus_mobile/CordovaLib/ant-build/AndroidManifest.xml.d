66fc47e6aca04bebe761b81403c477abe01abd68
xuan <czh-852@163.com>
2015-01-10 21:04:39 +0800

    删除该应用，重建phonegap工程

---
+++ kbus_mobile/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\yan\kbus_mobile\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\Users\yan\kbus_mobile\platforms\android\CordovaLib\AndroidManifest.xml \
---
+++ kbus_mobile/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\yan\kbus_mobile\platforms\android\ant-build\AndroidManifest.xml : \
+C:\Users\yan\kbus_mobile\platforms\android\AndroidManifest.xml \
